e7c3e5175559ebd6f10f06ef692f633f669f18a3
AndrewHypster <andrewgrechukh123@gmail.com>
2025-01-10 19:21:00 +0200

    router test

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 import Main from "/pages/main.js";
 import About from "/pages/about.js";
 
-const routes = {
-  "/": Main(),
-  "/about": About(),
-};
+const path = window.location.pathname;
+const root = document.getElementById("root");
 
-const renderRoute = () => {
-  const path = window.location.pathname;
-  const root = document.getElementById("root");
-  root.innerHTML = routes[path] ?? "<h1>404 Not Found</h1>";
-};
-
-// Викликаємо renderRoute під час завантаження
-window.onpopstate = renderRoute;
-document.addEventListener("DOMContentLoaded", () => {
-  renderRoute();
-});
+switch (path) {
+  case "/":
+    root.appendChild(Main());
+    break;
+  case "pages/about":
+    root.appendChild(About());
+    break;
+  default:
+    root.innerHTML = "<h1>Сторінка не знайдена</h1>";
+    break;
+}
